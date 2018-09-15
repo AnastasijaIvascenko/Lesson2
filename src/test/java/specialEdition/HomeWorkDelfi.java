53bb8f218bbd66618b4ccc871d30d9f1715365fa
anastasija <ivascenko.anastasija@gmail.com>
2018-09-15 16:39:01 +0300

    HomeWorkDelfi.ru

--- src/test/java/specialEdition/HomeWorkDelfi.java
+++ src/test/java/specialEdition/HomeWorkDelfi.java
@@ -19,11 +19,6 @@ public class HomeWorkDelfi {
     private final By REG_COMMENTS = By.xpath(".//*[@id='comments-listing']/div[3]/a[1]/span");
     private final By ANON_COMMENTS = By.xpath(".//*[@id='comments-listing']/div[3]/a[2]/span");
 
-
-
-
-
-
     @Test
     public void delfiTest() {
         System.setProperty("webdriver.gecko.driver", "/Users/anastasija/Desktop/QA2/geckodriver");
@@ -33,35 +28,26 @@ public class HomeWorkDelfi {
 
         //Первая страница
         driver.findElement(CLOSE_COOKIE).click();
-
         String title = driver.findElement(FIRST_TITLE).getText(); //запоминает текст и гетТекс вынос ит в текст без хпаса
         String comment = driver.findElement(COMMENT).getText();
         //то что отбросит скобки
         comment = comment.substring(1, comment.length() - 1); //отбрасывает первый и последний символ
         String com = comment;
         Integer com1 = Integer.valueOf(com);
-
         driver.findElement(FIRST_TITLE).click();
 
         //Вторая страница
-
-
-
         String articlePageTitle = driver.findElement(ARTICLE_PAGE_TITLE).getText();
         Assert.assertTrue(articlePageTitle.contains(title));
         //Assert.assertEquals("Wrong Title", title, driver.findElement(ARTICLE_PAGE_TITLE).getText());
         String articlePageComment = driver.findElement(ARTICLE_PAGE_COMMENT).getText();
         articlePageComment = articlePageComment.substring(1, articlePageComment.length() - 1);
-        //Assert.assertEquals("Wrong comment", comment, articlePageComment);
-
-
+        Assert.assertEquals("Wrong comment", comment, articlePageComment);
         driver.findElement(COMMENT).click();
 
         //Третья страница
-
         String partOfText = driver.findElement(LAST_PAGE_TITLE).getText();
         Assert.assertTrue(partOfText.contains(title));
-
         String regComment = driver.findElement(REG_COMMENTS).getText();
         regComment = regComment.substring(1, regComment.length()-1);
         String anonComment = driver.findElement(ANON_COMMENTS).getText();
@@ -71,11 +57,7 @@ public class HomeWorkDelfi {
         String anon = anonComment;
         Integer anon1 = Integer.valueOf(anon);
         Integer sum = (reg1 + anon1);
-
-
         Assert.assertEquals(com1, sum);
-
-
 
     }
 
